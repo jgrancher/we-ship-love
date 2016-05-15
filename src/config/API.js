7c47bda35720fcb502cc8b27955787f73357d620
Jeremy Grancher <jgrancher@gmail.com>
2016-05-15 15:52:32 +1000

    Added API informations in ./config/API.js

--- src/config/API.js
+++ src/config/API.js
@@ -1,4 +1,6 @@
 // Shopify variables
-export const API_HOSTNAME = '';
+// You can find them once you created your 'Private app' in the admin
+// https://storename.myshopify.com/admin/apps/private/
+export const API_HOSTNAME = ''; // storename.myshopify.com
 export const API_KEY = '';
 export const API_PASSWORD = '';
