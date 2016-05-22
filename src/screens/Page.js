01166b3badbb1ee515eab3094aa6bf16c4987283
Jeremy Grancher <jgrancher@gmail.com>
2016-05-23 00:35:33 +1000

    Pages: Added some styles into the content

--- src/screens/Page.js
+++ src/screens/Page.js
@@ -4,8 +4,16 @@ import { WebView } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+// Components
+import LoadingIndicator from '../components/LoadingIndicator';
+
 // Actions
 import { fetchPages } from '../actions/pages';
+
+// Styles
+import { brown, turquoise } from '../config/colors';
+import { spaceNormal } from '../config/sizes';
+import { source } from '../config/fonts';
 
 const { bool, func, string } = React.PropTypes;
 
@@ -23,12 +31,20 @@ class Page extends React.Component {
     }
 
     render() {
-        return (
-            <WebView
-                source={{ html: this.props.content }}
-                style={{ flex: 1 }}
-            />
-        );
+        if (this.props.isFetching) {
+            return <LoadingIndicator />;
+        }
+
+        const html =
+            `<style>
+                p { color: ${brown}; font-family: ${source}, sans-serif; font-size: 16px;
+                    text-align: justify; margin: 20px; }
+                strong { color: ${turquoise}; display: block; font-size: 22px;
+                    margin: ${spaceNormal}px; text-align: center; }
+            </style>
+            ${this.props.content}`;
+
+        return <WebView source={{ html }} />;
     }
 }
 
