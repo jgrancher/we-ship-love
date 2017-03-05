56e6d3536db159480123b3ff3e1538da09bad4d4
Jeremy Grancher <jgrancher@gmail.com>
2017-03-05 17:54:55 +1100

    Removed actions & reducer for ‘message’ since we’ve got redux-form 🎉

--- src/containers/Message/constants.js
+++
@@ -1,1 +0,0 @@
-export const SET_MESSAGE = 'SET_MESSAGE';
--- src/reducers.js
+++ src/reducers.js
@@ -4,7 +4,6 @@ import { reducer as forms } from 'redux-form';
 
 // Reducers
 import delivery from './containers/Delivery/reducer';
-import message from './containers/Message/reducer';
 import pages from './containers/Page/reducer';
 import products from './containers/Products/reducer';
 import shipping from './containers/Shipping/reducer';
@@ -13,7 +12,6 @@ import shop from './containers/App/reducer';
 const reducers = combineReducers({
   delivery,
   forms,
-  message,
   pages,
   products,
   shipping,
