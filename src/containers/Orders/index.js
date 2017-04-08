e4c0fc198cfa13878d33d1b4b12b77166f283c45
Jeremy Grancher <jgrancher@gmail.com>
2017-04-08 19:05:00 +1000

    Created Orders base component

---
+++ src/containers/Orders/index.js
@@ -0,0 +1,19 @@
+// Externals
+import React, { PropTypes } from 'react';
+import { View } from 'react-native';
+
+class Orders extends React.Component {
+
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <View />
+    );
+  }
+
+}
+
+export default Orders;
